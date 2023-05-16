4eb501ade51cdccb5b135031a08a04e26eccc6de
Monoxido45 <lucruz45.cab@gmail.com>
2023-05-15 22:41:27 -0300

    improving base test and adding plot function

--- R/base_test.R
+++ R/base_test.R
@@ -1,14 +1,17 @@
-#' REACT function for uni parametric precise hypothesis testing
+#' @title Uni parametric Agnostic Confidence-based precise test
+#' @description REACT function for uni parametric precise hypothesis testing
 #' @param CI parameter confidence interval for testing
-#' @param tol tolerance to build pragmatic hypothesis
+#' @param tol tolerance to build pragmatic hypothesis. It can be also an assymetric tolerance (vector of length 2)
 #' @param hip precise hypothesis of interest - default is 0
-#' @param plot set whether the test results should be plotted (plot = TRUE) or not (plot = FALSE), default is plot = TRUE
 #' @param verbose set whether text output should be generated (verbose = TRUE) or not (verbose = FALSE)
 #' @export
-
-base_test <- function(CI, tol, hip = 0, plot = TRUE, verbose = TRUE){
+base_test <- function(CI, tol, hip = 0, verbose = TRUE){
   # designing pragmatic interval
-  p_int <- c(hip - tol, hip + tol)
+  if(length(tol) == 1){
+    p_int <- c(hip - tol, hip + tol)
+  }else{
+    p_int <- c(hip - tol[1], hip + tol[2])
+  }
 
   # analysing whether CI falls into pragmatic region, or not or not entirely
   idxs <- findInterval(CI, p_int)
@@ -34,32 +37,45 @@ base_test <- function(CI, tol, hip = 0, plot = TRUE, verbose = TRUE){
             ifelse(test_outcome == "remain agnostic", ".", " the null hypothesis."))
   }
 
-  if(plot){
-    p <- ggplot2::ggplot()+
-      ggplot2::coord_cartesian(ylim=c(-0.1, 0.1)) +
-      ggplot2::geom_segment(ggplot2::aes(x = CI[1], y = 0,
-                                         xend = CI[2], yend = 0), linewidth = 0.75)+
-      ggplot2::annotate('rect', xmin = p_int[1], xmax = p_int[2],
-                        ymin = -0.05, ymax = 0.05, alpha=.3, fill='blue')+
-      ggplot2::theme_bw()+
-      ggplot2::theme(
-        panel.border = ggplot2::element_blank(),
-        panel.grid.major = ggplot2::element_blank(),
-        panel.grid.minor = ggplot2::element_blank(),
-        axis.line = ggplot2::element_line(colour = "black"),
-        axis.text.y = ggplot2::element_blank(),
-        axis.ticks.y = ggplot2::element_blank())+
-      ggplot2::labs(y = "",
-           x = "Parameter values",
-           title = "Hypothesis testing acceptance region and confidence interval")
 
-    show(p)
+  vals <- list(CI = CI,
+       pragmatic = p_int,
+       test_outcome = test_outcome)
 
-  }
+  class(vals) = c("simple_REACT")
 
-  return(list(CI = CI,
-              pragmatic = p_int,
-              test_outcome = test_outcome))
+  return(vals)
+}
+
+# TODO: multiple testing interface (if needed)
+
+
+
+#' @title Plot Region of equivalence and confidence interval of precise hypothesis test
+#' @param obj simple_REACT object to plot
+#' @export
+plot.simple_REACT <- function(obj){
+
+  p <- ggplot2::ggplot()+
+    ggplot2::coord_cartesian(ylim=c(-0.1, 0.1)) +
+    ggplot2::geom_segment(ggplot2::aes(x = obj$CI[1], y = 0,
+                                       xend = obj$CI[2], yend = 0), linewidth = 0.75)+
+    ggplot2::annotate('rect', xmin = obj$pragmatic[1], xmax = obj$pragmatic[2],
+                      ymin = -0.05, ymax = 0.05, alpha=.3, fill='blue')+
+    ggplot2::theme_bw()+
+    ggplot2::theme(
+      panel.border = ggplot2::element_blank(),
+      panel.grid.major = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.line = ggplot2::element_line(colour = "black"),
+      axis.text.y = ggplot2::element_blank(),
+      axis.ticks.y = ggplot2::element_blank())+
+    ggplot2::labs(y = "",
+                  x = "Parameter values",
+                  title = "Hypothesis testing acceptance region and confidence interval")
+
+  show(p)
+  invisible(p)
 }
 
 
--- data-raw/CI_tests.R
+++ data-raw/CI_tests.R
@@ -1,14 +1,18 @@
 # some tests using simple test function
 # CI falling inside region
+library(REACT)
 CI <- c(-0.5, 0.5)
-REACT::base_test(CI, tol = 0.3)
+obj <- REACT::base_test(CI, tol = 0.3)
+plot(obj)
 
 CI <- c(0, 0.25)
-REACT::base_test(CI, tol = 0.5)
+obj <- REACT::base_test(CI, tol = 0.5)
+plot(obj)
 
 CI <- c(-0.25, 0.25)
-REACT::base_test(CI, tol = 0.5)
+obj <- REACT::base_test(CI, tol = 0.5)
+plot(obj)
 
 CI <- c(0.5, 0.75)
-REACT::base_test(CI, tol = 0.3)
-
+obj <- REACT::base_test(CI, tol = 0.3)
+plot(obj)
