0fe0d1aa3bf4f6f1add5b67de3ee278f0d256ed1
Monoxido45 <lucruz45.cab@gmail.com>
2023-06-15 12:23:03 -0300

    improved ellipsis graphic using latex2exp

--- data-raw/camcog_test.R
+++ data-raw/camcog_test.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(ggplot2)
 library(ggforce)
+library(latex2exp)
 
 # importing data
 camcog <- read.csv("data-raw/CAMCOG.csv")
@@ -20,6 +21,8 @@ c <- 1
 alpha <- 0.05
 delta <- 10
 
+
+# Testing one ellipsis ----------------------------------------------------
 # testing ggplot2
 new_cov <- var_cov[c(1, 2), c(1, 2)]
 new_mu <- mu[c(1, 2)]
@@ -54,7 +57,9 @@ ggplot(aes(x = x, y = y), data = points_data) +
 
 
 
+# Multiple comparisons ----------------------------------------------------
 c = 1
+data_list = list()
 for(i in 1:(nrow(var_cov) - 1)){
   for(j in (i + 1):nrow(var_cov)){
     new_cov <- var_cov[c(i, j), c(i, j)]
@@ -75,24 +80,44 @@ for(i in 1:(nrow(var_cov) - 1)){
     mu_dif <- abs(new_mu[1] - new_mu[2])
 
     points <- DescTools::DrawEllipse(x = new_mu[1], y = new_mu[2], radius.x = a, radius.y = b,
-                                     rot = angle, plot = FALSE, nv = 4000)
+                                     rot = angle, plot = FALSE, nv = 1000)
 
     # points data
-    points_data <- data.frame(x = points$x,
-                              y = points$y)
-
-    plot_list[[c]] <- ggplot(aes(x = x, y = y), data = points_data) +
-      geom_polygon(colour="black", fill=NA)+
-      geom_abline(intercept = delta, linetype = "dashed")+
-      geom_abline(intercept = -delta, linetype = "dashed")+
-      coord_cartesian(xlim = c(new_mu[1] - mu_dif - delta, new_mu[1] + mu_dif + delta),
-                      ylim = c(new_mu[2] - mu_dif - delta, new_mu[2] + mu_dif + delta))+
-      labs(x = paste0("x_", i),
-           y = paste0("x_", j))
+    a = paste0("$\\theta_", i,  "$ and $\\theta_", j, "$")
+    data_list[[c]] <- data.frame(x = points$x,
+                              y = points$y,
+                              label = a)
 
     c = c + 1
   }
 }
+
+# concatenating data frame
+all_data <- do.call("rbind", data_list)
+
+x_min <- min(all_data$x) - delta
+x_max <- max(all_data$x) + delta
+
+# adding y to each x between x_min and x_max
+y_1 <- seq(x_min - delta, x_max - delta, length.out = 50)
+y_2 <- seq(x_min + delta, x_max + delta, length.out = 50)
+x <- seq(x_min, x_max, length.out = 50)
+prag_data <- data.frame(x = x,
+                        y_1 = y_1,
+                        y_2 = y_2)
+
+appender <- function(string){
+  TeX(string)}
+
+
+ggplot(aes(x = x, y = y), data = all_data) +
+  geom_polygon(colour = "black", fill=NA)+
+  geom_ribbon(aes(x = x, ymin = y_1, ymax = y_2),
+              data = prag_data, inherit.aes = FALSE,
+              fill = "dodgerblue3", alpha = 0.2) +
+  facet_wrap(~label, scales = "free", labeller = as_labeller(appender,
+                                                             default = label_parsed))
+
 
 ggpubr::ggarrange(plotlist = plot_list, nrow = 1, ncol = 3)
 
